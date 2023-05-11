411c11dbf859b3192b9ab6a4c9f4aba3a546ea4b
Victor V. Grekov <victor.grek@gmail.com>
2023-05-12 00:27:21 +0300

    api: small fix

--- Application/capture/Service/Handler/Handler.cs
+++ Application/capture/Service/Handler/Handler.cs
@@ -1,3 +1,10 @@
+using Capture.Service.Handler.provider;
+using Capture.Service.Listener;
+using Capture.Service.Parser;
+using Capture.Service.TaskQueue;
+using Database.Database;
+using Database.Models;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -6,116 +13,113 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Capture.Service.Handler.provider;
-using Capture.Service.Listener;
-using Capture.Service.Parser;
-using Capture.Service.TaskQueue;
-using Database.Database;
-using Database.Models;
-using Microsoft.Extensions.Logging;
 
 namespace Capture.Service.Handler;
 
 public class Handler : IHandler, IDisposable
 {
-    private readonly ILogger<Handler> _logger;
-    private readonly ICallsRepository _repository;
-    private readonly TaskQueue<UdpReceiveResult> _parseQueue;
-    private readonly BufferedTaskQueue<Data> _dbQueue;
-    private readonly IOptionsProvider _provider;
-    private readonly Timer _timer;
-    private Stopwatch _stopwatch = new Stopwatch();
+	private readonly ILogger<Handler> _logger;
+	private readonly ICallsRepository _repository;
+	private readonly TaskQueue<UdpReceiveResult> _parseQueue;
+	private readonly BufferedTaskQueue<Data> _dbQueue;
+	private readonly CustomBufferedQueue<string> _tmp;
+	private readonly IOptionsProvider _provider;
+	private readonly Timer _timer;
+	private Stopwatch _stopwatch = new Stopwatch();
 
-    private volatile int _counter = 0;
-    private volatile bool _isWatched = false;
+	private volatile int _counter = 0;
+	private volatile bool _isWatched = false;
 
-    private const int BufferSize = 1000;
+	private const int BufferSize = 1000;
 
-    public Handler(ILogger<Handler> logger, ICallsRepository repository, IOptionsProvider provider)
-    {
-        _logger = logger;
-        _repository = repository;
-        _parseQueue = new TaskQueue<UdpReceiveResult>(Parse, ParsingErrorHandler);
-        _dbQueue = new BufferedTaskQueue<Data>(Save, bufferSize: BufferSize, exceptionHandler: SavingErrorHandler);
-        _provider = provider;
-        _timer = new Timer((e) => { _logger.LogDebug("Parser queue size {0}", _parseQueue.TaskCount); }, null,
-            TimeSpan.Zero, TimeSpan.FromSeconds(60));
-    }
+	public Handler(ILogger<Handler> logger, ILogger<CustomBufferedQueue<string>> logger2, ICallsRepository repository, IOptionsProvider provider)
+	{
+		_logger = logger;
+		_repository = repository;
+		_parseQueue = new TaskQueue<UdpReceiveResult>(Parse, ParsingErrorHandler);
+		_dbQueue = new BufferedTaskQueue<Data>(Save, bufferSize: BufferSize, exceptionHandler: SavingErrorHandler);
+		_provider = provider;
+		_timer = new Timer((e) => { _logger.LogDebug("Parser queue size {0}", _parseQueue.TaskCount); }, null,
+			TimeSpan.Zero, TimeSpan.FromSeconds(60));
 
-    private void Parse(UdpReceiveResult r)
-    {
-        var data = new ReceivedData(r.Buffer, r.RemoteEndPoint, DateTime.Now);
-        var message = ParserHePv3.ParseMessage(data.Msg);
-        var sipMethod = Enum.Parse<SipMethods>(message.Sip.CSeqMethod.ToString());
-        if (!_provider.GetExcludedMethods().Contains(sipMethod))
-        {
-            var d = GetData(message, data.EndPoint, data.Time);
-            _dbQueue.EnqueueTask(d);
-        }
-    }
+		_tmp = new CustomBufferedQueue<string>(_ => Task.CompletedTask, logger2);
+	}
 
-    private void ParsingErrorHandler(Exception e, UdpReceiveResult y)
-    {
-        _logger.LogWarning("Error parsing message: {0}", e.Message);
-    }
+	private void Parse(UdpReceiveResult r)
+	{
+		var data = new ReceivedData(r.Buffer, r.RemoteEndPoint, DateTime.Now);
+		var message = ParserHePv3.ParseMessage(data.Msg);
+		var sipMethod = Enum.Parse<SipMethods>(message.Sip.CSeqMethod.ToString());
+		if (!_provider.GetExcludedMethods().Contains(sipMethod))
+		{
+			var d = GetData(message, data.EndPoint, data.Time);
+			_dbQueue.EnqueueTask(d);
+		}
+	}
 
-    private void SavingErrorHandler(Exception e, IList<Data> y)
-    {
-        _logger.LogWarning("Error saving message: {0}", e.Message);
-    }
+	private void ParsingErrorHandler(Exception e, UdpReceiveResult y)
+	{
+		_logger.LogWarning("Error parsing message: {0}", e.Message);
+	}
 
-    private async Task Save(IList<Data> data)
-    {
-        // _stopwatch.Start();
-        await _repository.InsertRangeAsync(data);
-        // Interlocked.Add(ref _counter, BufferSize);
-        // _logger.LogCritical(", {1}", _stopwatch.ElapsedMilliseconds / 1000.0);
-    }
-    
-    public void HandleMessage(UdpReceiveResult r)
-    {
-        _parseQueue.EnqueueTask(r);
-    }
+	private void SavingErrorHandler(Exception e, IList<Data> y)
+	{
+		_logger.LogWarning("Error saving message: {0}", e.Message);
+	}
 
-    private Data GetData(Message msg, IPEndPoint endPoint, DateTime time)
-    {
-        Dictionary<string, string> headers = new();
+	private async Task Save(IList<Data> data)
+	{
+		// _stopwatch.Start();
+		await _repository.InsertRangeAsync(data);
+		// Interlocked.Add(ref _counter, BufferSize);
+		// _logger.LogCritical(", {1}", _stopwatch.ElapsedMilliseconds / 1000.0);
+	}
 
-        foreach (var h in msg.Sip.UnknownHeaders)
-        {
-            var (key, value) = ParseUnknownHeader(h);
-            if (_provider.GetAvailableHeaders().Contains(key))
-            {
-                headers[key] = value;
-            }
-        }
+	public void HandleMessage(UdpReceiveResult r)
+	{
+		_parseQueue.EnqueueTask(r);
+		_tmp.Push(r.RemoteEndPoint.Address.ToString());
+	}
 
-        var details = new Details(
-            msg.Hep.sourceIPAddress,
-            msg.Hep.destinationIPAddress,
-            msg.Hep.timeSeconds,
-            msg.Hep.timeUseconds);
+	private Data GetData(Message msg, IPEndPoint endPoint, DateTime time)
+	{
+		Dictionary<string, string> headers = new();
 
-        return new Data(
-            headers,
-            Encoding.Default.GetString(msg.Payload),
-            msg.Sip.CallId,
-            endPoint,
-            time,
-            details);
-    }
+		foreach (var h in msg.Sip.UnknownHeaders)
+		{
+			var (key, value) = ParseUnknownHeader(h);
+			if (_provider.GetAvailableHeaders().Contains(key))
+			{
+				headers[key] = value;
+			}
+		}
 
-    private static (string, string) ParseUnknownHeader(string str)
-    {
-        var x = str.Split(':');
-        return (x[0].Trim(), x[1].Trim());
-    }
+		var details = new Details(
+			msg.Hep.sourceIPAddress,
+			msg.Hep.destinationIPAddress,
+			msg.Hep.timeSeconds,
+			msg.Hep.timeUseconds);
 
-    public void Dispose()
-    {
-        _timer.Change(Timeout.Infinite, 0);
-        _timer.Dispose();
-        _parseQueue?.Dispose();
-        _dbQueue?.Dispose();
-    }
+		return new Data(
+			headers,
+			Encoding.Default.GetString(msg.Payload),
+			msg.Sip.CallId,
+			endPoint,
+			time,
+			details);
+	}
+
+	private static (string, string) ParseUnknownHeader(string str)
+	{
+		var x = str.Split(':');
+		return (x[0].Trim(), x[1].Trim());
+	}
+
+	public void Dispose()
+	{
+		_timer.Change(Timeout.Infinite, 0);
+		_timer.Dispose();
+		_parseQueue?.Dispose();
+		_dbQueue?.Dispose();
+	}
 }
--- Application/capture/Service/Listener/UdpCapture.cs
+++ Application/capture/Service/Listener/UdpCapture.cs
@@ -1,76 +1,75 @@
-﻿using System;
-using System.IO;
+﻿using Capture.Service.Handler;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
-using Capture.Service.Handler;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Capture.Service.Listener
 {
-    public class UdpCapture : ICapture
-    {
-        private readonly CaptureConfiguration _config;
-        private readonly ILogger<UdpCapture> _logger;
-        private CancellationTokenSource _cts;
-        private IHandler _handler;
+	public class UdpCapture : ICapture
+	{
+		private readonly CaptureConfiguration _config;
+		private readonly ILogger<UdpCapture> _logger;
+		private CancellationTokenSource _cts;
+		private IHandler _handler;
 
-        private UdpClient _listener;
-        private UdpClient _homerClient;
-        private Task _task;
+		private UdpClient _listener;
+		private UdpClient _homerClient;
+		private Task _task;
 
-        private int _count = 0;
-        
-        public UdpCapture(IConfiguration config, ILogger<UdpCapture> logger, IHandler handler)
-        {
-            _logger = logger;
-            _config = config.GetSection("Capture").Get<CaptureConfiguration>();
-            _handler = handler;
-        }
+		private int _count = 0;
 
-        public void Dispose()
-        {
-            _listener.Dispose();
-            _homerClient?.Dispose();
-            _cts?.Dispose();
-        }
+		public UdpCapture(IConfiguration config, ILogger<UdpCapture> logger, IHandler handler)
+		{
+			_logger = logger;
+			_config = config.GetSection("Capture").Get<CaptureConfiguration>();
+			_handler = handler;
+		}
 
-        public async Task StartAsync(CancellationToken ct)
-        {
-            _cts = CancellationTokenSource.CreateLinkedTokenSource(ct);
+		public void Dispose()
+		{
+			_listener.Dispose();
+			_homerClient?.Dispose();
+			_cts?.Dispose();
+		}
 
-            var listenEndpoint = new IPEndPoint(IPAddress.Any, _config.ListenPort);
+		public async Task StartAsync(CancellationToken ct)
+		{
+			_cts = CancellationTokenSource.CreateLinkedTokenSource(ct);
 
-            _listener = new UdpClient(listenEndpoint);
+			var listenEndpoint = new IPEndPoint(IPAddress.Any, _config.ListenPort);
 
-            _logger.LogInformation("Start udp listener");
-            _task = CaptureAsync();
-        }
+			_listener = new UdpClient(listenEndpoint);
 
-        private async Task reсeive()
-        {
-            var x = await _listener.ReceiveAsync(_cts.Token);
-            _handler.HandleMessage(x);
-        }
-        
-        private async Task CaptureAsync()
-        {
-            var counter = 1;
-            try
-            {
-                while (!_cts.Token.IsCancellationRequested)
-                {
-                    await reсeive();
-                    counter += 1;
-                    if (counter % 100 == 0) Console.WriteLine(counter);
-                }
-            }
-            catch (Exception e)
-            {
-                _logger.LogWarning(e.Message);
-            }
-        }
-    }
+			_logger.LogInformation("Start udp listener");
+			_task = CaptureAsync();
+		}
+
+		private async Task Reсeive()
+		{
+			var x = await _listener.ReceiveAsync(_cts.Token);
+			_handler.HandleMessage(x);
+		}
+
+		private async Task CaptureAsync()
+		{
+			var counter = 1;
+			try
+			{
+				while (!_cts.Token.IsCancellationRequested)
+				{
+					await Reсeive();
+					counter += 1;
+					if (counter % 100 == 0) Console.WriteLine(counter);
+				}
+			}
+			catch (Exception e)
+			{
+				_logger.LogWarning(e.Message);
+			}
+		}
+	}
 }
